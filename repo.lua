77dba212b3323dec55cf436ea87cfcf984e424a7
Zenobius Jiricek <airtonix@gmail.com>
2013-07-10 01:17:15 +0930

    correctly store lua table for loadstring

--- repo.lua
+++ repo.lua
@@ -1,10 +1,10 @@
-{
-	repo = "Airtonix Computer Craft Repository",
+repo = {
+	name = "Airtonix Computer Craft Repository",
 	homepage = "https://github.com/airtonix/computer-craft-applications/",
 	packages = {
 		"daemon",
 		"internal-navigation",
-		"tubes"
-		"application-template"
+		"tubes",
+		"application-template",
 	}
 }
